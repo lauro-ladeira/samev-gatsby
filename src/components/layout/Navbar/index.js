fe5398e8252cc790e97e1573f47f2a6d748860ac
Clara Pagotto <clarapagotto@gmail.com>
2020-07-28 11:57:51 -0300

    navbar responsiva

--- src/components/layout/Navbar/index.js
+++ src/components/layout/Navbar/index.js
@@ -1,19 +1,43 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import * as S from "./styled"
 
 function Navbar() {
+  const [active, setActive] = useState(false)
+
+  const toggleClassName = () => {
+    const currentState = active
+    setActive(!currentState)
+  }
+
   return (
     <S.NavbarWrapper>
       <S.NavbarContainer>
         <S.LogoContainer></S.LogoContainer>
-        <S.MenuContainer>
-          <S.MenuItem to="/">Início</S.MenuItem>
-          <S.MenuItem to="/a-samev">A SAMEV</S.MenuItem>
-          <S.MenuItem to="/programacao">Programação</S.MenuItem>
-          <S.MenuItem to="/inscricao">Inscrição</S.MenuItem>
-          <S.MenuItem to="/resumos">Resumos</S.MenuItem>
-          <S.MenuItem to="/contato">Contato</S.MenuItem>
+        <S.MenuContainer active={active ? 'activeMenu' : null}>
+          <S.MenuItem>
+            <S.MenuLink to="/" activeClassName="active">Início</S.MenuLink>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.MenuLink to="/a-samev" activeClassName="active">A SAMEV</S.MenuLink>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.MenuLink to="/programacao" activeClassName="active">Programação</S.MenuLink>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.MenuLink to="/inscricao" activeClassName="active">Inscrição</S.MenuLink>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.MenuLink to="/resumos" activeClassName="active">Resumos</S.MenuLink>
+          </S.MenuItem>
+          <S.MenuItem>
+            <S.MenuLink to="/contato" activeClassName="active">Contato</S.MenuLink>
+          </S.MenuItem>
         </S.MenuContainer>
+        <S.Burger onClick={toggleClassName}>
+          <S.BurgerLine active={active ? "activeLine1" : null}></S.BurgerLine>
+          <S.BurgerLine active={active ? "activeLine2" : null}></S.BurgerLine>
+          <S.BurgerLine active={active ? "activeLine3" : null}></S.BurgerLine>
+        </S.Burger>
       </S.NavbarContainer>
     </S.NavbarWrapper>
   )
--- src/components/layout/Navbar/styled.js
+++ src/components/layout/Navbar/styled.js
@@ -1,17 +1,22 @@
 import styled, { css } from "styled-components"
 import { Link } from "gatsby"
+import media from "styled-media-query"
 
 export const NavbarWrapper = styled.div`
   align-items: center;
   background-color: #060761;
   display: flex;
-  height: 100px;
+  height: 80px;
   justify-content: center;
 `
 export const NavbarContainer = styled.nav`
   display: flex;
   justify-content: space-between;
   width: 75%;
+
+  ${media.lessThan("large")`
+    width: 85%;
+  `}
 `
 export const LogoContainer = styled.div`
   background-color: #f6ab00;
@@ -19,25 +24,94 @@ export const LogoContainer = styled.div`
 `
 
 export const MenuContainer = styled.ul`
-  display: flex;
-  justify-content: space-between;
+  ${media.lessThan("804px")`
+    align-items: center;
+    background-color: #effeff;
+    display: flex;
+    flex-direction: column;
+    height: calc(100vh - 80px);
+    justify-content: space-around;
+    left: 100%;
+    margin: 0 25%;
+    position: fixed;
+    top: 80px;
+    transition: ease-out left .3s;
+    width: 75%;
+    z-index: 99;
 
-  ${props =>
-    props.active === "activeMenu" &&
-    css`
-      left: 0;
-    `}
+    ${props =>
+      props.active === "activeMenu" &&
+      css`
+        left: 0;
+      `}
+  `}
 `
 
-export const MenuItem = styled(Link)`
+export const MenuItem = styled.li`
+  display: inline-block;
+
+  ${media.lessThan("804px")`
+    display: block;
+    width: 90%;
+  `}
+`
+
+export const MenuLink = styled(Link)`
   color: #fff;
   font-family: "Abel";
   font-size: 16px;
   text-decoration: none;
+  text-align: center;
   padding: 10px 20px;
 
   &:active,
   &:hover {
     color: #f6ab00;
   }
+
+  ${media.lessThan("804px")`
+    display: block;
+    color: #060761;
+    border-bottom: 1px solid #ddd;
+    font-size: 20px;
+    padding: 6% 20px;
+
+    &:hover, &.active {
+      font-weight: bold;
+  `}
+`
+export const Burger = styled.div`
+  display: none;
+  margin: 26px;
+  cursor: pointer;
+
+  ${media.lessThan("804px")`
+    display: block;
+  `}
+`
+
+export const BurgerLine = styled.div`
+  width: 25px;
+  height: 3px;
+  margin: 5px;
+  background-color: #fff;
+  transition: all .3s ease;
+
+  ${props =>
+    props.active === "activeLine1" &&
+    css`
+      transform: rotate(-45deg) translate(-5px, 6px);
+    `}
+
+  ${props =>
+    props.active === "activeLine2" &&
+    css`
+      opacity: 0;
+    `}
+
+  ${props =>
+    props.active === "activeLine3" &&
+    css`
+      transform: rotate(45deg) translate(-5px, -6px);
+    `}
 `
