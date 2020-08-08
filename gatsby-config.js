3afaf7bc3fcc4d739c42424b468daa41548ea521
Lauro <lauro.floresta@gmail.com>
2020-08-08 18:55:34 -0300

    deploy test

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sharp`
     `gatsby-plugin-styled-components`,
     {
       resolve: "gatsby-plugin-web-font-loader",
